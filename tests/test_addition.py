aa4f9a6c866fdddc7cd690c51948e0391cc115c3
leoknave <perezlea@kean.edu>
2025-02-05 12:05:36 -0500

    Fixed trailing whitespace in test_addition.py

--- tests/test_addition.py
+++ tests/test_addition.py
@@ -2,7 +2,7 @@
 Unit tests for the add function in the Calculator class.
 """
 
-from app.calculator import Calculator  
+from app.calculator import Calculator  # Importing the Calculator class
 
 def test_add():
     """Tests the addition method of Calculator."""
