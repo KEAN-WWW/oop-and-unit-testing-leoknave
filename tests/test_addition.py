3adf8cf745932f28d1c5a767455e1adcd059667b
leoknave <122905241+leoknave@users.noreply.github.com>
2025-02-05 11:09:50 -0500

    Update test_addition.py

--- tests/test_addition.py
+++ tests/test_addition.py
@@ -1,12 +1,7 @@
-"""
-Unit tests for the add function in the Calculator class.
-"""
-
 import pytest
 from app.calculator import Calculator
 
 def test_add():
-    """Tests the addition method of Calculator."""
     calc = Calculator()
     assert calc.add(2, 3) == 5
     assert calc.add(-1, 1) == 0
